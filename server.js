7a76ce23177e0c38764c58babd59b33692298c33
kienn <duckien86@gmail.com>
2017-03-29 22:23:03 +0700

    initialize

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var port = 9000;
+
+var middleware = require('./middleware.js');
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+    res.send('About us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, function () {
+    console.log('server start on port:' + port);
+});
